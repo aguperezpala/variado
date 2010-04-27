92c397476a7f68175e5d40be35a4971a29f834b0
aguperezpala <aguperezpala@5df5296c-e330-11de-b950-c1e2c078a2c5>
2010-04-27 23:02:18 +0000

    antes de modificar btconnection - btsimpleserver

--- trunk/bt-manager/src/consts.h
+++ trunk/bt-manager/src/consts.h
@@ -4,5 +4,8 @@
 /*! Maximo numero de conexiones por dongle */
 #define MAX_CON_PER_DONGLE	8
 
+/*! Funcion que determina minimo y maximo */
+#define min(X, Y)  ((X) < (Y) ? (X) : (Y))
+#define max(X, Y)  ((X) > (Y) ? (X) : (Y))
 
 #endif
